ac16c86e432ea94c5d47fef23ce7295ff9fb8511
gaukharmu <goowwaa@gmail.com>
2024-06-03 20:54:39 +0500

    add axios

--- nuxt.config.js
+++ nuxt.config.js
@@ -46,3 +46,26 @@ module.exports = {
     }
   }
 }
+
+
+export default {
+  modules: [
+    '@nuxtjs/axios'
+  ],
+
+  axios: {
+    baseURL: 'http://localhost:4000' // Used as fallback if no runtime config is provided
+  },
+
+  publicRuntimeConfig: {
+    axios: {
+      browserBaseURL: process.env.BROWSER_BASE_URL
+    }
+  },
+
+  privateRuntimeConfig: {
+    axios: {
+      baseURL: process.env.BASE_URL
+    }
+  }
+}
