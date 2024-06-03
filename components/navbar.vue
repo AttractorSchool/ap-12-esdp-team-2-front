a6ba50fcbc8e8389c685121bf6489c89ab6df515
gaukharmu <goowwaa@gmail.com>
2024-06-03 14:45:13 +0500

    add navbar

---
+++ components/navbar.vue
@@ -0,0 +1,29 @@
+<template>
+  <nav class="navbar bg-body-tertiary mt-3">
+    <div class="container-fluid">
+      <nuxt-link class="nav-link" to="/">
+        <img src="@/static/logo.png" alt="logo img" class="logo">
+      </nuxt-link>
+      <nuxt-link class="nav-link" to="#">
+        <img src="@/static/login.png" alt="login img" class="login me-3">
+      </nuxt-link>
+    </div>
+  </nav>
+</template>
+<script setup lang="ts">
+</script>
+
+<style>
+.logo {
+  width: 300px;
+  height: 90px;
+}
+
+.login {
+  width: 40px;
+  height: 40px;
+}
+.search {
+  width: 500px;
+}
+</style>
