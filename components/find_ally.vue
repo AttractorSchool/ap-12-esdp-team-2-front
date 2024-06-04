55c7348872ff133e0cd976a3519a4d047ea3c4cb
gaukharmu <goowwaa@gmail.com>
2024-06-04 23:38:33 +0500

    add find ally component

---
+++ components/find_ally.vue
@@ -0,0 +1,37 @@
+<template>
+  <section class="mt-5 mb-5">
+    <div class="container d-flex justify-content-center">
+      <div class="row">
+        <div class="col-6 my-img-card">
+          <img src="@/static/images/ally.jpg" alt="ally img" class="object-fit-cover col-12 ">
+        </div>
+        <div class="col-5 my-card-body ms-5">
+          <h4 class="my-card-title ms-2"><br> Единомышленники в один клик: <br> ваш путь к новым связям</h4>
+          <p class="my-card-title ms-3">
+            Наше приложение предоставляет уникальную платформу, где вы можете найти тех,
+            кто разделяет ваши взгляды и увлечения, с легкостью и комфортом. Откройте новые
+            горизонты дружбы и сотрудничества с нашим быстрым и простым поиском единомышленников.
+          </p>
+          <a href="#" class="btn btn-success ms-4">ПОИСК ЕДИНОМЫШЛЕННИКОВ</a>
+        </div>
+      </div>
+    </div>
+  </section>
+
+</template>
+
+<script setup lang="ts">
+</script>
+
+<style scoped>
+
+  .my-card-body {
+    border-top: solid 12px #f26f43;
+    border-bottom: solid 12px #f26f43;
+    border-right: solid 12px #f26f43;
+    border-radius: 20px;
+  }
+  .my-card-title {
+    color: black;
+  }
+</style>
