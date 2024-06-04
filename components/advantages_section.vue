17e69acac61de5c9b803602f8cc91ea932945f8e
gaukharmu <goowwaa@gmail.com>
2024-06-04 21:35:13 +0500

    change advantages component

--- components/advantages_section.vue
+++ components/advantages_section.vue
@@ -1,35 +1,31 @@
 <template>
-   <section class="u-align-center u-clearfix u-grey-10 u-section-6" id="carousel_d1d0">
-      <div class="u-align-center u-container-align-center u-container-style u-expanded-width u-group u-palette-3-base u-group-1">
-        <div class="u-container-layout u-valign-top u-container-layout-1">
-          <h2 class="u-align-center u-text u-text-1">
-            Наши приемущества
-          </h2>
-          <p class="u-align-center u-text u-text-2">
-            Здесь вы обязательно найдете что-то для себя
-          </p>
-        </div>
-      </div>
-      <div class="u-gallery u-layout-grid u-lightbox u-no-transition u-show-text-on-hover u-gallery-1">
-        <div class="u-gallery-inner u-gallery-inner-1">
-          <div class="u-effect-fade u-gallery-item">
-            <div class="u-back-slide">
-              <img class="u-back-image u-expanded" src="@/static/images/advantages1.png">
-            </div>
-          </div>
-          <div class="u-effect-fade u-gallery-item">
-            <div class="u-back-slide">
-              <img class="u-back-image u-expanded" src="@/static/images/advantages2.png">
-            </div>
-          </div>
-          <div class="u-effect-fade u-gallery-item">
-            <div class="u-back-slide">
-              <img class="u-back-image u-expanded" src="../static/images/advantages3.png">
-            </div>
-          </div>
-        </div>
-      </div>
+   <section class="u-align-center u-clearfix u-grey-10 u-section-6 block_advantages pb-5">
+
+     <div class="pt-5">
+       <h2 class="advantages_text">Наши приемущества</h2>
+       <p class="advantages_text">Здесь вы обязательно найдете что-то для себя</p>
+     </div>
+     <div class="container">
+           <div class="row align-items-center">
+             <img class="col-4 img object-fit-cover m-2" src="@/static/images/advantages1.png">
+             <img class="col-4 img object-fit-cover" src="@/static/images/advantages2.png">
+             <img class="col-4 img object-fit-cover" src="../static/images/advantages3.png">
+           </div>
+         </div>
     </section>
 </template>
 <script setup lang="ts">
 </script>
+
+<style scoped>
+.img {
+  width: 400px;
+  height: 700px;
+}
+.block_advantages {
+  background-image: linear-gradient(#f26f43 50%, transparent 50%);
+}
+.advantages_text {
+  color: white;
+}
+</style>
