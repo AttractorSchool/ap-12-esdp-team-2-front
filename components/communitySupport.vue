1998f7ab8b79c1be4c30fb096b1200f31bd8f487
gaukharmu <goowwaa@gmail.com>
2024-06-09 21:36:24 +0500

    add component community support

---
+++ components/communitySupport.vue
@@ -0,0 +1,73 @@
+<template>
+  <section class="top-category-widget-area pt-90 pb-90 ">
+    <div class="container">
+      <div class="row d-flex justify-content-center">
+        <div class="menu-content pb-70 col-lg-8">
+          <div class="title text-center">
+            <h1 class="mb-10">Поддержка для сообществ</h1>
+            <p>
+              Мы предлагаем широкий спектр услуг поддержки,
+              разработанных специально для содействия развитию и
+              процветанию вашего сообщества. Наша миссия состоит в том,
+              чтобы стать вашим надёжным партнёром в стремлении создать
+              сильное, взаимодействующее и вдохновляющее сообщество.
+            </p>
+          </div>
+        </div>
+      </div>
+      <div class="row">
+        <div class="col-lg-4">
+          <div class="single-cat-widget">
+            <div class="content relative">
+              <div class="overlay overlay-bg"></div>
+                <a href="#" target="_blank">
+                  <div class="thumb">
+                   <img class="content-image img-fluid d-block mx-auto" src="@/static/img/services/atributika.jpg" alt=""  style="height: 250px;">
+                  </div>
+                  <div class="content-details">
+                    <h4 class="content-title mx-auto text-uppercase">Клубная атрибутика</h4>
+                    <span></span>
+                    <p class="w-75 mx-auto">Печать и вышивка логотипов, картинок, надписей и т.д.</p>
+                  </div>
+                </a>
+            </div>
+          </div>
+        </div>
+        <div class="col-lg-4">
+          <div class="single-cat-widget">
+            <div class="content relative">
+              <div class="overlay overlay-bg"></div>
+                <a href="#" target="_blank">
+                  <div class="thumb">
+                   <img class="content-image img-fluid d-block mx-auto" src="@/static/img/services/consult.jpg" alt="" style="height: 250px;">
+                  </div>
+                  <div class="content-details">
+                    <h4 class="content-title mx-auto text-uppercase">Консультация</h4>
+                    <span></span>
+                    <p class="w-75 mx-auto">Мы постараемся ответить на ваши вопросы, развеять сомнения там, где они были.</p>
+                  </div>
+                </a>
+            </div>
+          </div>
+        </div>
+        <div class="col-lg-4">
+          <div class="single-cat-widget">
+            <div class="content relative">
+              <div class="overlay overlay-bg"></div>
+                <a href="#" target="_blank">
+                  <div class="thumb">
+                   <img class="content-image img-fluid d-block mx-auto" src="@/static/img/services/audience.jpg" alt=""  style="height: 250px;">
+                  </div>
+                  <div class="content-details">
+                    <h4 class="content-title mx-auto text-uppercase">Организация мероприятии</h4>
+                    <span></span>
+                    <p class="w-75 mx-auto">Перфекционизм в каждом мероприятии, обеспечиваем яркие впечатления.</p>
+                  </div>
+                </a>
+            </div>
+          </div>
+        </div>
+      </div>
+    </div>
+  </section>
+</template>
--- pages/index.vue
+++ pages/index.vue
@@ -1,16 +1,22 @@
 <template>
-  <Banner/>
-  <UnderBanner/>
+  <div style="margin-bottom: 600px">
+    <Banner/>
+    <UnderBanner/>
+    <CommunitySupport/>
+  </div>
+
 
 </template>
 
 <script>
-  import Banner from '@/components/banner.vue'
+  import Banner from '@/components/banner.vue';
   import UnderBanner from "@/components/underBanner.vue";
+  import CommunitySupport from "@/components/communitySupport.vue";
   export default {
     components: {
       Banner,
-      UnderBanner
+      UnderBanner,
+      CommunitySupport
     }
   }
 </script>
