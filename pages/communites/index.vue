73c0a68eaeeaf8dff093f37395df29bd7e4a4743
Zhantemir1 <zhantik.k@gmail.com>
2024-06-10 20:32:36 +0500

    create tamplate

---
+++ pages/communites/index.vue
@@ -0,0 +1,28 @@
+<script setup lang="ts">
+
+</script>
+
+<template>
+  <div>
+    <Banner/>
+    <div class="d-flex align-content-center" style="height: 500px">
+      <h1>Community List</h1>
+    </div>
+  </div>
+
+
+</template>
+
+<script>
+  import Banner from '@/components/banner.vue';
+
+  export default {
+    components: {
+      Banner,
+    }
+  }
+</script>
+
+<style scoped>
+
+</style>
