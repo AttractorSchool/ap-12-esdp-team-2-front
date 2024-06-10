6027c31c6b18772d025397673bcd92199c4d0b43
Zhantemir1 <zhantik.k@gmail.com>
2024-06-11 02:03:30 +0500

    feature community list page

---
+++ components/miniBanner.vue
@@ -0,0 +1,23 @@
+<script setup lang="ts">
+
+</script>
+
+<template>
+    <section class="banner-area relative about-banner" id="home">
+      <div class="overlay overlay-bg"></div>
+      <div class="container">
+        <div class="row d-flex align-items-center justify-content-center">
+          <div class="about-content col-lg-12">
+            <h1 class="text-white">
+              Popular Communities
+            </h1>
+            <p class="text-white link-nav"><a href="/">Home </a>  <span class="lnr lnr-arrow-right"></span>  <a href="/communities"> Popular Communities</a></p>
+          </div>
+        </div>
+      </div>
+    </section>
+</template>
+
+<style scoped>
+
+</style>
