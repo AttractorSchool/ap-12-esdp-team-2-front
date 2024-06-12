637e5fa72dc427601c7801809c67f4a6259c9954
Aziza <lespekova.aziiza@gmail.com>
2024-06-12 20:06:21 +0500

    fix about pages

---
+++ pages/about.vue
@@ -0,0 +1,28 @@
+<template>
+  <div>
+    <BannerAbout/>
+    <UnderBannerAbout/>
+    <InformationSection/>
+    <MissionAbout/>
+    <FormAbout/>
+  </div>
+
+
+</template>
+
+<script>
+  import BannerAbout from '@/components/about/bannerAbout.vue';
+  import UnderBannerAbout from '@/components/about/underBannerAbout.vue';
+  import InformationSection from '@/components/about/informationSection.vue';
+  import MissionAbout from '@/components/about/missionAbout.vue';
+  import FormAbout from '@/components/about/formAbout.vue';
+  export default {
+    components: {
+      BannerAbout,
+      UnderBannerAbout,
+      InformationSection,
+      MissionAbout,
+      FormAbout,
+    }
+  }
+</script>
